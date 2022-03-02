72a5b5a5766d8fa02d5d748a6ffd0c7c523a7abd
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-02 18:48:12 +0900

    Update main.py
    Рассчитал время полета.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import xml.dom.minidom as minidom
+
+from datetime import datetime
 from pprint import pprint
+
+
+
+def get_format_date(raw_date):
+    date, time = raw_date.split("T")
+    processed_date = date +  f'T{time[:2]}:{time[2:]}'
+    processed_date = datetime.fromisoformat(processed_date)
+    return processed_date
+
+
+def get_flight_time(departure, arrival):
+    departure = get_format_date(departure)
+    arrival = get_format_date(arrival)
+    delta = (arrival - departure).total_seconds()
+    return delta
 
 
 def main():
@@ -7,18 +24,25 @@ def main():
     dom.normalize()
 
     elements = dom.getElementsByTagName("Flight")
-    currency_dict = {}
+    flights = {}
 
     for node in elements:
         for child in node.childNodes:
             if child.nodeType == 1:
                 if child.tagName == 'Carrier':
                     if child.firstChild.nodeType == 3:
-                        currency_dict['Carrier'] = child.firstChild.data
+                        flights['carrier'] = child.firstChild.data
                 if child.tagName == 'FlightNumber':
                     if child.firstChild.nodeType == 3:
-                        currency_dict['Flight Number'] = child.firstChild.data
-        pprint(currency_dict)
+                        flights['code'] = child.firstChild.data
+                if child.tagName == 'DepartureTimeStamp':
+                    if child.firstChild.nodeType == 3:
+                        departure = child.firstChild.data
+                if child.tagName == 'ArrivalTimeStamp':
+                    if child.firstChild.nodeType == 3:
+                        arrival = child.firstChild.data
+                        flights['flght_time'] = get_flight_time(departure, arrival)
+        print(flights)
 
 
 if __name__ == '__main__':
