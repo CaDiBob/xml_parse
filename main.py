b368a361de5ed1fde8d28ae8b2d64c19ca0d5d7d
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-06 11:47:21 +0900

    Update main.py
    - Добавил вывод общего количества рейсов
    - Добавил аргумент командной строки

--- main.py
+++ main.py
@@ -1,8 +1,65 @@
+import argparse
 import xml.dom.minidom as minidom
 
 from datetime import datetime
 from pprint import pprint
 
+
+def add_argument_parser():
+    parser = argparse.ArgumentParser(
+        description='Парсит xml файлы и выводит данные по ним'
+    )
+    parser.add_argument(
+        '--count',
+        help='Количество рейсов'
+    )
+    return parser
+
+
+def get_flights(xml_file):
+    flights = list()
+    dom = minidom.parse(xml_file)
+    dom.normalize()
+    elements = dom.getElementsByTagName("Flight")
+    for node in elements:
+        flight = dict()
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'Carrier':
+                    if child.firstChild.nodeType == 3:
+                        flight['carrier'] = child.firstChild.data
+                if child.tagName == 'FlightNumber':
+                    if child.firstChild.nodeType == 3:
+                        flight['code'] = child.firstChild.data
+                if child.tagName == 'DepartureTimeStamp':
+                    if child.firstChild.nodeType == 3:
+                        departure = child.firstChild.data
+                if child.tagName == 'ArrivalTimeStamp':
+                    if child.firstChild.nodeType == 3:
+                        arrival = child.firstChild.data
+                        flight['flght_time'] = get_flight_time(
+                            departure,
+                            arrival,
+                            )
+        flights.append(flight)
+    return flights
+
+
+def get_price_ticket(xml_file):
+    prices = list()
+    dom = minidom.parse(xml_file)
+    dom.normalize()
+    elements = dom.getElementsByTagName("Pricing")
+    for node in elements:
+        price = list()
+        for child in node.childNodes:
+            if child.nodeType == 1:
+                if child.tagName == 'ServiceCharges':
+                    if child.firstChild.nodeType == 3:
+                        price.append(float(child.firstChild.data))
+
+        prices.append(max(price))
+    return prices
 
 
 def get_format_date(raw_date):
@@ -20,29 +77,17 @@ def get_flight_time(departure, arrival):
 
 
 def main():
-    dom = minidom.parse('RS_Via-3.xml')
-    dom.normalize()
+    parser = add_argument_parser()
+    args = parser.parse_args()
+    flights_1 = get_flights('RS_ViaOW.xml')
+    flights_2 = get_flights('RS_Via-3.xml')
+    prices_1 = get_price_ticket('RS_ViaOW.xml')
+    prices_2 = get_price_ticket('RS_Via-3.xml')
+    flights_keys = [flight.get('code') for flight in flights_1]
+    diff = [flight for flight in flights_2 if not flight.get('code') in flights_keys]
 
-    elements = dom.getElementsByTagName("Flight")
-    flights = {}
-
-    for node in elements:
-        for child in node.childNodes:
-            if child.nodeType == 1:
-                if child.tagName == 'Carrier':
-                    if child.firstChild.nodeType == 3:
-                        flights['carrier'] = child.firstChild.data
-                if child.tagName == 'FlightNumber':
-                    if child.firstChild.nodeType == 3:
-                        flights['code'] = child.firstChild.data
-                if child.tagName == 'DepartureTimeStamp':
-                    if child.firstChild.nodeType == 3:
-                        departure = child.firstChild.data
-                if child.tagName == 'ArrivalTimeStamp':
-                    if child.firstChild.nodeType == 3:
-                        arrival = child.firstChild.data
-                        flights['flght_time'] = get_flight_time(departure, arrival)
-        print(flights)
+    if args.count:
+        print(len(flights_1)+len(flights_2))
 
 
 if __name__ == '__main__':
